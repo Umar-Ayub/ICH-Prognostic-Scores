b42b9e62e38f7b1cd83c08c272bc920c5d11d971
Umar-Ayub <umarayub28@gmail.com>
2024-06-17 16:23:24 -0700

    mapping fix

--- create_mapping.py
+++ create_mapping.py
@@ -61,18 +61,3 @@ column_mapping = {
     'ICH_FOS_score': 'ICH_FOS_score  - check score components and see if we can get it from ATACh data',
     'Max_ICH_score': 'Max_ICH_score  - check score components and see if we can get it from ATACh data'
 }
-
-# Identify columns in Dataset 2 that do not map to Dataset 1
-dataset2_columns = set([
-    'SUBJECT_ID', 'AGE (F33Q02)', 'GENDER F01Q01 (male 1; female 0)', 'ETHNICITY - at the end of the sheet', 'NIHSS at admission is F10 Q22 in Form 10. Use score prior to randomization denoted as " - "', 'Glucose (mg/dl) (F15Q11)  Use score prior to randomization denoted as " - "', 'SBP (prior to randomization) (F05Q09a)', 'DBP (prior to randomization) 9F05Q09b)', 'PRIOR ANTIHTN  (F04Q01)', 'PRIOR ANTIDM (F04Q03)', 'DM ADM F03Q11 and 12 (type 1 and 2)', 'HTN ADM (F03Q07)', 'HLD (F03Q09) (yes=1, no=2, unknwon =98)', 'STROKE Hx (F03Q01) (defined as prior stroke/TIA Hx)', 'AF (F03Q04)', 'CAD (F3Q06)', 'SMOKING (F03Q13)  (defined as current=1,former=2,never=3,unknown=4)', 'PLT  (x 10^3 / mm^3) (F15Q07)  Use score prior to randomization denoted as " - "', 'INR (F15Q09)  Use score prior to randomization denoted as " - "', 'APTT (sec) (F15Q08)  Use score prior to randomization denoted as " - "', 'EVD F07Q05 (called intraventricular catheter received)', 'HCM F07Q08 (defined as immediate hematoma evac)', 'ICUCARE, F1Q01 represent Days in ICU --> Made into  1=Yes and 0= no', 'INTUBATED (used  F08Q04 ; Not F07Q02)', 'Site of Bleed (F2Q07)  1=BG, 2=thalamus, 3=lobar) (pontine / cerebellar excluded) (Use variable 45 in Qatar: 1=BG, 2=thalamus, use 3 & 6 to count as lobar, exclude pts with 4,5,7)', 'GCSADM F33Q04', 'VOLUME 1 ( on admission) in mm3 -CTCR Q 12', 'IVH (F33QO3) (1 = present , 0==absent)', 'HEM STATUS ON REPEAT CT (0=no change, 1=increased size, 2= decreased size) --> calculate from volume 1 and 2 and change into cm3', 'MORT 90 - (F20Q04 MIN)', 'MRS90 (0 - mRS 0-3 , 1 - mRS 4-6)  (Form 21 primary outcome )', 'AGE > 80   (ask Statistician for help)', 'oICH_score', 'mICH_score', 'ICH_GS_score', 'LSICH_score', 'ICH_FOS_score', 'Max_ICH_score'
-])
-unmapped_columns = dataset2_columns - set(column_mapping.values())
-
-# Display the mapping and unmapped columns
-print("Column Mapping from Dataset 1 to Dataset 2:")
-for key, value in column_mapping.items():
-    print(f"{key}: {value}")
-
-print("\nColumns in Dataset 2 that did not map to Dataset 1:")
-for column in sorted(unmapped_columns):
-    print(column)
